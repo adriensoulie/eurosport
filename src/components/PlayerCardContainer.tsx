e53b6553e4ec504297a9e0433e89fa231fd6f8b5
adriensoulie <adrien@morphos.is>
2022-06-20 00:48:35 +0700

    added player stats title

--- src/components/PlayerCardContainer.tsx
+++ src/components/PlayerCardContainer.tsx
@@ -5,7 +5,10 @@ export default function PlayerCardContainer({ setLoading, isLoading }: any) {
   const playersInfos = GetPlayersInfos();
 
   return (
-    <div>
+    <div className="mt-16">
+      {playersInfos && (
+        <h1 className="mt-4 text-4xl font-bold text-center">Players stats</h1>
+      )}
       <div className="flex justify-center">
         {playersInfos?.players.map((player) => {
           return <PlayerCard {...player} />;
